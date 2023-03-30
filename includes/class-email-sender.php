333637d42fec11599ac21e4b5e507ff05ab79a1b
sonamdivyanshi1 <sonam.divyanshi@wisdmlabs.com>
2023-03-30 13:17:28 +0530

    corrected class object

--- includes/class-email-sender.php
+++ includes/class-email-sender.php
@@ -168,7 +168,7 @@ class Email_Sender {
 	 */
 	private function define_public_hooks() {
 
-		$plugin_public = new Email_Sender_Public( $this->get_plugin_name(), $this->get_version() );
+		$plugin_public = new Email_Sender_Admin( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
